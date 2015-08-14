dd337bcdf3fa74d6279a60d06b573783bf5a9528
kevTheDev <kev.j.edwards@gmail.com>
2015-08-14 17:03:42 +0100

    subscriber model

---
+++ app/models/subscriber.rb
@@ -0,0 +1,5 @@
+class Subscriber < ActiveRecord::Base
+  
+  validates :email, presence: true, uniqueness: true
+  
+end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,10 +1,11 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Subscriber do
 
   describe 'validations' do
 
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
 
   end
   # validations
