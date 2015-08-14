8e6adec991e1775cbd94c8cef4fcf0e181c29c2b
kevTheDev <kev.j.edwards@gmail.com>
2015-08-14 16:49:52 +0100

    bare bones admin CRUD on articles

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,21 +1,44 @@
 class Admin::ArticlesController < Admin::BaseController
   
   def index
+    @articles = Article.all.page(params[:page])
   end
   
   def new
+    @article = Article.new
   end
   
   def create
+    @article = Article.new(article_params)
+    
+    if @article.save
+      redirect_to(admin_articles_path, notice: 'Article Created')
+    else
+      render action: 'new'
+    end
   end
   
   def edit
+    @article = Article.friendly.find(params[:id])
   end
   
   def update
+    @article = Article.friendly.find(params[:id])
+    
+    if @article.update_attributes(article_params)
+      redirect_to(admin_articles_path, notice: 'Article Updated')
+    else
+      render action: 'edit'
+    end
   end
   
   def destroy
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
   
 end
--- spec/controllers/admin/articles_controller_spec.rb
+++ spec/controllers/admin/articles_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Admin::ArticlesController do
   
+  let(:article) { create(:article) }
+  
   context 'guest user' do
     
     describe 'GET index' do
@@ -11,9 +13,28 @@ describe Admin::ArticlesController do
       end
     
       it { should respond_with(401) }
-    
     end
     # GET index
+    
+    describe 'GET edit' do
+    
+      before do
+        get :edit, id: article.id
+      end
+    
+      it { should respond_with(401) }
+    end
+    # GET edit
+    
+    describe 'GET new' do
+    
+      before do
+        get :new
+      end
+    
+      it { should respond_with(401) }
+    end
+    # GET new
     
   end
   # guest user
@@ -24,8 +45,8 @@ describe Admin::ArticlesController do
       http_login
     end
     
-    let(:article) { create(:article) }
-  
+    let(:valid_params) { { article: { title: 'test', body: 'test' } } }
+    
     describe 'GET index' do
     
       before do
@@ -37,6 +58,45 @@ describe Admin::ArticlesController do
     
     end
     # GET index
+    
+    describe 'GET new' do
+      
+      before do
+        get :new
+      end
+    
+      it { should respond_with(:ok) }
+      it { should render_template('new') }
+      
+    end
+    # GET new
+    
+    describe 'POST create' do
+      
+      context 'failed save' do
+        
+        before do
+          allow_any_instance_of(Article).to receive(:save) { false }
+          post :create, valid_params
+        end
+        
+        it { should render_template('new')}
+      end
+      # failed save
+      
+      context 'successful save' do
+        
+        before do
+          allow_any_instance_of(Article).to receive(:save) { true }
+          post :create, valid_params
+        end
+        
+        it { should respond_with(:redirect) }
+      end
+      # successful save
+      
+    end
+    # POST create
   
     describe 'GET edit' do
       
@@ -49,6 +109,35 @@ describe Admin::ArticlesController do
       
     end
     # GET edit
+    
+    describe 'PUT update' do
+      
+      context 'failed save' do
+        
+        before do
+          allow_any_instance_of(Article).to receive(:save) { false }
+          put :update, valid_params.merge(id: article.id)
+        end
+        
+        it { should render_template('edit')}
+      end
+      # failed save
+      
+      context 'successful save' do
+        
+        before do
+          allow_any_instance_of(Article).to receive(:save) { true }
+          put :update, valid_params.merge(id: article.id)
+        end
+        
+        it { should respond_with(:redirect) }
+      end
+      # successful save
+      
+    end
+    # PUT update
+    
+    
     
   end
   # authenticated user
